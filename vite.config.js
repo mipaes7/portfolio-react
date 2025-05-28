e13361a76e57c79f446b3c345b64651a7736809e
MPardal <miguelpardal.esparis@ext.essilor.com>
2025-05-28 16:22:40 +0200

    github pages deploy config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+const myRepo = 'portfolio-react'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: `${myRepo}`
 })
